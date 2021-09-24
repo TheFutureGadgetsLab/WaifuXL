f39cd1c0efbcccf8e226e1a9b546c8e3c8220e2f
Alec Benson <alecbenson21@gmail.com>
2021-09-23 19:59:48 -0600

    Simple beta navbar, new pages for about and donate

---
+++ pages/donate/index.js
@@ -0,0 +1,6 @@
+export default function Home() {
+    return (
+        <>About </>
+    )
+}
+
--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,18 @@ import { useState, useEffect, createRef } from "react";
 import { drawImage } from "../services/imageUtilities";
 import { initializeONNX } from "../services/onnxBackend";
 import { clearCanvas } from "../services/canvasUtilities";
+import { PINK } from "../constants/colors";
 import CanvasComponent from "../components/CanvasComponent";
 import DownloadComponent from "../components/DownloadComponent";
 import TitleComponent from "../components/TitleComponent";
 import InputComponent from "../components/InputComponent";
 import RunComponent from "../components/RunComponent";
-
+import Router from "next/router";
 export default function Home() {
-  const loadingLink = "https://thumbs.gfycat.com/ThunderousScratchyArthropods.webp";
+  const loadingLink =
+    "https://thumbs.gfycat.com/ThunderousScratchyArthropods.webp";
   const canvasRef = createRef();
   const outputCanvasRef = createRef();
-
   const [canvasContexts, setCanvasContexts] = useState({
     input: undefined,
     output: undefined,
@@ -50,49 +51,71 @@ export default function Home() {
   }, []);
 
   return (
-    <div
-      className="flex flex-col items-center justify-center min-h-screen py-2"
-      style={{ backgroundImage: `url("bg.png")`, backgroundSize: "cover" }}
-    >
-      <CanvasComponent
-        width={width}
-        height={height}
-        canvasRef={canvasRef}
-        outputCanvasRef={outputCanvasRef}
-        loadingImgSrc={loadingLink}
-        loading={loading}
-      />
-      <br />
-      {showDownloads && (
-        <DownloadComponent
+    <>
+      <div
+        className="flex flex-col items-center min-h-screen"
+        style={{ backgroundImage: `url("bg.png")`, backgroundSize: "cover" }}
+      >
+        <div class="fixed">
+          <header>
+            <nav class="">
+              <div class="container mx-auto py-4 flex justify-between items-center">
+                <h1 class="text-2xl font-bold text-black" style={{ color: PINK }}>WaifuXL</h1>
+                <div class="flex px-10 space-x-10">
+                  <div class="flex items-center space-x-2" onClick={() => Router.push("/about")}>
+                    <span class="text-black">About</span>
+                  </div>
+                  <div class="flex items-center space-x-2" onClick={() => Router.push("/donate")}>
+                    <span class="text-black">Donate</span>
+                  </div>
+                </div>
+              </div>
+            </nav>
+          </header>
+        </div>
+        <br />
+        <br />
+        <br />
+
+        <CanvasComponent
+          width={width}
+          height={height}
           canvasRef={canvasRef}
           outputCanvasRef={outputCanvasRef}
-          url={url}
+          loadingImgSrc={loadingLink}
+          loading={loading}
         />
-      )}
-      <TitleComponent loading={loading} />
-      <div className="grid grid-cols-2 gap-3 py-2 px-4">
-        <InputComponent
-          open={open}
-          setOpen={setOpen}
-          canvasContexts={canvasContexts}
-          setHeight={setHeight}
-          setWidth={setWidth}
-          url={url}
-          setShowDownloads={setShowDownloads}
-          setUrl={setUrl}
-        />
-        <RunComponent
-          canvasContext={canvasContexts.input}
-          outputCanvasContext={canvasContexts.output}
-          setShowDownloads={setShowDownloads}
-          setLoading={setLoading}
-          setHeight={setHeight}
-          setWidth={setWidth}
-          height={height}
-          width={width}
-        />
+        {showDownloads && (
+          <DownloadComponent
+            canvasRef={canvasRef}
+            outputCanvasRef={outputCanvasRef}
+            url={url}
+          />
+        )}
+        <TitleComponent loading={loading} />
+        <div className="grid grid-cols-2 gap-3 py-2 px-4">
+          <InputComponent
+            open={open}
+            setOpen={setOpen}
+            canvasContexts={canvasContexts}
+            setHeight={setHeight}
+            setWidth={setWidth}
+            url={url}
+            setShowDownloads={setShowDownloads}
+            setUrl={setUrl}
+          />
+          <RunComponent
+            canvasContext={canvasContexts.input}
+            outputCanvasContext={canvasContexts.output}
+            setShowDownloads={setShowDownloads}
+            setLoading={setLoading}
+            setHeight={setHeight}
+            setWidth={setWidth}
+            height={height}
+            width={width}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
