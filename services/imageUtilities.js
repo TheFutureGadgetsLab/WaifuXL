86ea0cc46a6bcc24c8906a68170e8faabeb90c48
Alec Benson <alecbenson21@gmail.com>
2021-12-29 20:06:58 -0700

    Implemented fix for Firefox CORS issue

--- services/imageUtilities.js
+++ services/imageUtilities.js
@@ -1,3 +1,15 @@
+function isValidHttpUrl(string) {
+  let url;
+
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+
+  return url.protocol === "http:" || url.protocol === "https:";
+}
+
 export function getPixelsFromInput(input) {
   return new Promise((resolve, reject) => {
     const img = new Image();
@@ -5,31 +17,41 @@ export function getPixelsFromInput(input) {
     img.crossOrigin = "Anonymous";
     var results = null;
     img.onload = function () {
-      const canvas = document.createElement('canvas');
+      const canvas = document.createElement("canvas");
       canvas.width = img.width;
       canvas.height = img.height;
-      const context = canvas.getContext('2d');
+      const context = canvas.getContext("2d");
       context.drawImage(img, 0, 0);
       resolve(context.getImageData(0, 0, img.width, img.height));
-    }
-  })
+    };
+  });
 }
 
 export function getDataURIFromInput(input) {
-  return new Promise((resolve, reject) => {
-    const img = new Image();
-    img.src = input;
-    img.crossOrigin = "Anonymous";
-    var results = null;
-    img.onload = function () {
-      const canvas = document.createElement('canvas');
-      canvas.width = img.width;
-      canvas.height = img.height;
-      const context = canvas.getContext('2d');
-      context.drawImage(img, 0, 0);
-      resolve(canvas.toDataURL());
+  return new Promise(async (resolve, reject) => {
+    if (isValidHttpUrl(input)) {
+      let blob = await fetch(input).then(r => r.blob());
+      let dataUrl = await new Promise(resolve => {
+        let reader = new FileReader();
+        reader.onload = () => resolve(reader.result);
+        reader.readAsDataURL(blob);
+      });
+      resolve(dataUrl);
+    } else {
+      const img = new Image();
+      img.src = input;
+      img.crossOrigin = "Anonymous";
+      var results = null;
+      img.onload = function () {
+        const canvas = document.createElement("canvas");
+        canvas.width = img.width;
+        canvas.height = img.height;
+        const context = canvas.getContext("2d");
+        context.drawImage(img, 0, 0);
+        resolve(canvas.toDataURL());
+      };
     }
-  })
+  });
 }
 
 export function downloadImage(postFix, inputURI, downloadURI) {
@@ -44,8 +66,8 @@ export function downloadImage(postFix, inputURI, downloadURI) {
 export function getDataURIFromFileUpload(uploaded, setDataURI) {
   const file = uploaded;
   const fr = new FileReader();
-  fr.onload = function() {
-      setDataURI(fr.result);
-  }
+  fr.onload = function () {
+    setDataURI(fr.result);
+  };
   fr.readAsDataURL(file);
 }
