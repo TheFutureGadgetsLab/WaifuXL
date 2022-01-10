b52dda5fc69f38153d5e320ea2b00eb870285cea
Alec Benson <alecbenson21@gmail.com>
2022-01-09 20:43:16 -0700

    Made input modal easier to escape with an icon, and clicking anywhere in the background also closing the modal

--- components/ModalComponent.jsx
+++ components/ModalComponent.jsx
@@ -42,6 +42,10 @@ function ModalComponent({
         <div
           className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
           aria-hidden="true"
+          onClick={(e) => {
+            setInputModalOpen(false);
+            setFileName();
+          }}
         ></div>
 
         <span
@@ -52,6 +56,23 @@ function ModalComponent({
         </span>
 
         <div className="inline-block align-bottom bg-white rounded-lg shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+          <div className="">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 0 24 24"
+              width="24px"
+              fill="#000000"
+              className="mt-3 mr-3 float-right"
+              onClick={(e) => {
+                setInputModalOpen(false);
+                setFileName();
+              }}
+            >
+              <path d="M0 0h24v24H0V0z" fill="none" />
+              <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" />
+            </svg>
+          </div>
           <span className="block text-gray-700 pt-3">
             Click preview to open file dialogue
           </span>
@@ -108,7 +129,8 @@ function ModalComponent({
               </select>
             </label>
 
-            <button id="done-button"
+            <button
+              id="done-button"
               type="button"
               className="rounded-md absolute m-3 right-0 bottom-0 text-blue shadow-sm px-4 py-1 
                 text-base font-medium h-12 focus:outline-none focus:ring-2 focus:ring-offset-2 
