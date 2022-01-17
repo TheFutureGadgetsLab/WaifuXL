a45515d89ad9d0c0cd9e0fd0b2c7ec7e11135dbd
Alec Benson <alecbenson21@gmail.com>
2022-01-16 23:39:39 -0700

    Small style change to the about pageg

--- pages/about/index.js
+++ pages/about/index.js
@@ -10,7 +10,7 @@ export default function About() {
       <main className="flex flex-col items-center flex-shrink justify-center w-5/12 p-10 text-center">
         <div className="mt-20 text-6xl">At a Glance</div>
         <div className="mt-5 text-lg text-left">
-          <a href="https://www.waifuxl.com" className="text-pink">
+          <a href="https://www.waifuxl.com" className="text-pink underline">
             WaifuXL
           </a>{" "}
           provides state of the art upscaling{" "}
@@ -24,7 +24,7 @@ export default function About() {
           Hi, we’re the{" "}
           <span className="text-blue font-bold">Future Gadgets Lab</span>! We
           make a lot of random stuff, check out our organization{" "}
-          <a className="text-pink" href="https://github.com/TheFutureGadgetsLab">
+          <a className="text-pink underline" href="https://github.com/TheFutureGadgetsLab">
             here
           </a>
           .
@@ -35,32 +35,32 @@ export default function About() {
           the{" "}
           <a
             href="https://onnxruntime.ai/"
-            className="text-pink"
+            className="text-pink underline"
           >
             ONNX Runtime
           </a>
           . For the upscaling model we’re using the{" "}
-          <a href="https://arxiv.org/abs/2107.10833" className="text-pink">
+          <a href="https://arxiv.org/abs/2107.10833" className="text-pink underline">
             SOTA Real-ESRGAN
           </a>
           . Our tagger is a{" "}
-          <a href="https://arxiv.org/abs/1905.02244" className="text-pink">
+          <a href="https://arxiv.org/abs/1905.02244" className="text-pink underline">
             MobileNetV3
           </a>
           . On the web side we’re using{" "}
-          <a className="text-pink" href="https://reactjs.org/">
+          <a className="text-pink underline" href="https://reactjs.org/">
             React
           </a>
           ,{" "}
-          <a className="text-pink" href="https://nextjs.org/">
+          <a className="text-pink underline" href="https://nextjs.org/">
             Next.js
           </a>
           , and{" "}
-          <a className="text-pink" href="https://tailwindcss.com/">
+          <a className="text-pink underline" href="https://tailwindcss.com/">
             TailwindCSS
           </a>
           . We’re hosted on{" "}
-          <a className="text-pink" href="https://pages.cloudflare.com/">
+          <a className="text-pink underline" href="https://pages.cloudflare.com/">
             cloudflare pages
           </a>
           , which generously provides unlimited bandwith.
