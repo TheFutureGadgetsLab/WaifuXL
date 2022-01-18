3a40c2acdd7b6f191751447535ee8be32440348e
Alec Benson <alecbenson21@gmail.com>
2022-01-17 19:54:11 -0700

    Maybe finally got mobile layout working well

--- components/ImageDisplayComponent.jsx
+++ components/ImageDisplayComponent.jsx
@@ -5,7 +5,7 @@ import {
   
 const ImageDisplay = ({ outputURI, inputURI }) => {
   return (
-      <div className="h-5/6 flex items-center justify-center w-full">
+      <div className="h-4/6 flex items-center justify-center w-full">
         {outputURI == null ? (
           <img src={inputURI} className="border-pink border-4 lg:h-4/6 md:w-auto w-full" />
         ) : (
