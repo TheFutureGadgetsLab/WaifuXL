536491ae85e940a2e7745d96c371ac37d74861ec
Alec Benson <alecbenson21@gmail.com>
2022-01-23 21:12:41 -0700

    Built simple donation page, also refactored some of the icons on the page for mobile, as well as modifying the navbar to use a tags instead of spans

--- components/NavbarComponent.jsx
+++ components/NavbarComponent.jsx
@@ -7,28 +7,25 @@ const NavbarComponent = () => {
         <nav className="">
           <div className="container mx-auto py-4 flex justify-between items-center">
             <div className="flex px-10 gap-4">
-              <div
-                className="grow md:flex md:items-center hidden"
-                onClick={() => Router.push("./about")}
-              >
-                <span className="text-2xl font-semibold text-black cursor-pointer">
+              <div className="grow md:flex md:items-center hidden">
+                <a
+                  href="/about"
+                  className="text-2xl font-semibold text-black cursor-pointer"
+                >
                   About
-                </span>
+                </a>
               </div>
-              <div
-                className="grow text-3xl font-bold cursor-pointer text-pink"
-                onClick={() => Router.push("./")}
-              >
-                WaifuXL
+              <div className="grow text-3xl font-bold cursor-pointer text-pink">
+                <a href="/">WaifuXL</a>
               </div>
 
               <div
                 className="grow md:flex md:items-center hidden"
                 onClick={() => Router.push("./donate")}
               >
-                <span className="text-2xl font-semibold text-black cursor-pointer">
+                <a href="/donate" className="text-2xl font-semibold text-black cursor-pointer">
                   Donate
-                </span>
+                </a>
               </div>
             </div>
           </div>
