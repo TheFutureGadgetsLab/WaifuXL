cea9905d6d368d41ff14d782c8be99b76433344a
Alec Benson <alecbenson21@gmail.com>
2022-05-13 22:05:54 -0600

    Haydn is a CSS master and fixed styling maybe?

--- components/ImageDisplayComponent.jsx
+++ components/ImageDisplayComponent.jsx
@@ -1,24 +1,28 @@
 import {
-    ReactCompareSlider,
-    ReactCompareSliderImage,
-  } from "react-compare-slider";
-  
+  ReactCompareSlider,
+  ReactCompareSliderImage,
+} from "react-compare-slider";
+
 const ImageDisplay = ({ outputURI, inputURI }) => {
   return (
-      <div className="h-5/6 flex items-center justify-center w-full drop-shadow-md overflow-hidden">
-        {outputURI == null ? (
-          <img src={inputURI} className="lg:h-5/6 md:w-auto w-full" />
-        ) : (
-          <ReactCompareSlider
-            className="lg:h-5/6 m-1"
-            position={50}
-            itemOne={<ReactCompareSliderImage src={inputURI} alt="Image one" />}
-            itemTwo={
-              <ReactCompareSliderImage src={outputURI} alt="Image two" />
-            }
-          />
-        )}
-      </div>
+    <div className="h-5/6 flex items-center justify-center w-full drop-shadow-md overflow-hidden">
+      {outputURI == null ? (
+        <img src={inputURI} className="h-5/6 md:w-auto w-full" />
+      ) : (
+        <ReactCompareSlider
+          className="h-5/6 md:w-auto m-1"
+          position={50}
+          itemOne={<ReactCompareSliderImage src={inputURI} alt="Image one" />}
+          itemTwo={
+            <ReactCompareSliderImage src={outputURI} alt="Image two" />
+          }
+          style={{
+            width:"75vh",
+            height:"75vh"
+          }}
+        />
+      )}
+    </div>
   );
 };
 
