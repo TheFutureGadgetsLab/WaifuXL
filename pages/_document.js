deb3ec967ad476474ad74e78faecab81e03d6533
Haydn Jones <haydnjonest@gmail.com>
2022-05-22 12:37:51 -0600

    Updates to images / paths

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="manifest" href="/manifest.json" />
-          <link rel="apple-touch-icon" href="/icon.png"></link>
+          <link rel="apple-touch-icon" href="/images/icon.png"></link>
           <meta name="theme-color" content="#fff" />
         </Head>
         <body>
