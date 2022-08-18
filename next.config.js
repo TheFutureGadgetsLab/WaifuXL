66a180d3c45ede30a4f530549246b498de135f3e
Haydn Jones <haydnjonest@gmail.com>
2022-08-17 21:21:54 -0600

    Fixed wasm copy

--- next.config.js
+++ next.config.js
@@ -1,21 +1,22 @@
 const CopyPlugin = require("copy-webpack-plugin");
 const withPWA = require("next-pwa");
+const path = require("path");
 
 module.exports = withPWA({
   reactStrictMode: true,
   webpack: (config, {  }) => {
     config.plugins.push(
     new CopyPlugin({
-      patterns: [
-        {
-          from: './node_modules/onnxruntime-web/dist/ort-wasm.wasm',
-          to: 'static/chunks/pages',
-        },             {
-          from: './node_modules/onnxruntime-web/dist/ort-wasm-simd.wasm',
-          to: 'static/chunks/pages',
-        },          
+        patterns: [
+          {
+            from: path.join(
+              __dirname,
+              "node_modules/onnxruntime-web/dist/*.wasm"
+            ),
+            to: path.join(__dirname, ".next/static/chunks/pages/[name][ext]"),
+          },
         ],
-      }),
+      })
     );
 
     return config;
