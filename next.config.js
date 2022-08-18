99c9a58474d0dcd9bc7c54d0b6809c532a4ffdaa
Haydn Jones <haydnjonest@gmail.com>
2022-08-17 20:53:55 -0600

    Huge important changes to webpack, christ reincarnated

--- next.config.js
+++ next.config.js
@@ -1,33 +1,21 @@
-const isProd = process.env.NODE_ENV === "production";
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-  /* config options here */
-};
-
-const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 const withPWA = require("next-pwa");
 
 module.exports = withPWA({
-  assetPrefix: ".",
-  basePath: isProd ? "" : "",
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Important: return the modified config
+  reactStrictMode: true,
+  webpack: (config, {  }) => {
     config.plugins.push(
-      new CopyPlugin({
-        // Use copy plugin to copy *.wasm to output folder.
-        patterns: [
-          {
-            from: path.join(
-              __dirname,
-              "node_modules/onnxruntime-web/dist/*.wasm"
-            ),
-            to: path.join(__dirname, ".next/static/chunks/pages/[name][ext]"),
-          },
+    new CopyPlugin({
+      patterns: [
+        {
+          from: './node_modules/onnxruntime-web/dist/ort-wasm.wasm',
+          to: 'static/chunks/pages',
+        },             {
+          from: './node_modules/onnxruntime-web/dist/ort-wasm-simd.wasm',
+          to: 'static/chunks/pages',
+        },          
         ],
-      })
+      }),
     );
 
     return config;
