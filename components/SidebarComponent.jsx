68e5301d25536a4bedca370eb3932282ef418de0
Alec Benson <alecbenson21@gmail.com>
2022-08-25 22:51:07 -0600

    Refactored sidebar

--- components/SidebarComponent.jsx
+++ components/SidebarComponent.jsx
@@ -1,19 +1,16 @@
 import DownloadComponent from './DownloadComponent'
 import RunComponent from './RunComponent'
-import InputComponent from './InputComponent'
 import TagComponent from './TagComponent'
 import Router from 'next/router'
 import ScreenIcons from './ScreenIconsComponent'
 import { useImageStore, useAppStateStore } from '../services/useState'
+import { UploadSVG } from './SVGComponents'
 
 const Sidebar = () => {
   const showSidebar = useAppStateStore((state) => state.showSidebar)
   const outputURI = useImageStore((state) => state.outputURI)
-  const fileName = useImageStore((state) => state.fileName)
   const tags = useImageStore((state) => state.tags)
 
-  const setUpscaleFactor = useImageStore((state) => state.setUpscaleFactor)
-  const setFileName = useImageStore((state) => state.setFileName)
   return (
     <div id="sidebar" className="w-80 flex flex-col fixed inset-y-0 z-20">
       <div
@@ -24,34 +21,10 @@ const Sidebar = () => {
         <div className="flex-1 flex flex-col overflow-y-auto">
           <div className="">
             <div className="pt-5 mt-10 mb-10 mx-8 grid grid-cols-1 space-y-2">
-              <div className="md:hidden flex items-center space-x-2" onClick={() => Router.push('./about')}>
-                <span className="text-2xl font-semibold text-black cursor-pointer">About</span>
-              </div>
-              <div className="md:hidden flex items-center space-x-2" onClick={() => Router.push('./donate')}>
-                <span className="text-2xl font-semibold text-black cursor-pointer">Donate</span>
-              </div>
+              <MobileNavLinksComponent />
               <hr className="md:hidden" />
               <InputComponent />
-              {outputURI != null ? (
-                <DownloadComponent />
-              ) : (
-                <div id="upscale-button-container" className="flex justify-between gap-2">
-                  <RunComponent />
-                  <select
-                    id="resolution-select"
-                    className="form-select appearance-none border-none text-white font-bold py-2 px-4
-                      rounded drop-shadow-lg bg-pink inline-flex items-center w-16"
-                    onInput={(inp) => {
-                      setUpscaleFactor(parseInt(inp.target.value))
-                      setFileName(fileName)
-                    }}
-                  >
-                    <option value="2">2&#215;</option>
-                    <option value="4">4&#215;</option>
-                    <option value="8">8&#215;</option>
-                  </select>
-                </div>
-              )}
+              {outputURI != null ? <DownloadComponent /> : <UpscaleContainer />}
             </div>
             {tags != null && (
               <>
@@ -65,6 +38,64 @@ const Sidebar = () => {
         </div>
       </div>
     </div>
+  )
+}
+
+function MobileNavLinksComponent() {
+  return (
+    <>
+      <div className="md:hidden flex items-center space-x-2" onClick={() => Router.push('./about')}>
+        <span className="text-2xl font-semibold text-black cursor-pointer">About</span>
+      </div>
+      <div className="md:hidden flex items-center space-x-2" onClick={() => Router.push('./donate')}>
+        <span className="text-2xl font-semibold text-black cursor-pointer">Donate</span>
+      </div>
+    </>
+  )
+}
+
+function UpscaleContainer() {
+  return (
+    <div id="upscale-button-container" className="flex justify-between gap-2">
+      <RunComponent />
+      <UpscaleFactorComponent />
+    </div>
+  )
+}
+
+function UpscaleFactorComponent() {
+  const setUpscaleFactor = useImageStore((state) => state.setUpscaleFactor)
+
+  return (
+    <select
+      id="resolution-select"
+      className="form-select appearance-none border-none text-white font-bold py-2 px-4 rounded drop-shadow-lg bg-pink inline-flex items-center w-16"
+      onInput={(inp) => {
+        setUpscaleFactor(parseInt(inp.target.value))
+      }}
+    >
+      <option value="2">2&#215;</option>
+      <option value="4">4&#215;</option>
+      <option value="8">8&#215;</option>
+    </select>
+  )
+}
+
+const InputComponent = () => {
+  const setInputModalOpen = useAppStateStore((state) => state.setInputModalOpen)
+  return (
+    <>
+      <button
+        id="choose-image-button"
+        type="button"
+        className="hover:bg-blue-700 text-white font-bold py-2 px-4 rounded drop-shadow-lg bg-pink inline-flex items-center"
+        onClick={() => {
+          setInputModalOpen(true)
+        }}
+      >
+        <UploadSVG /> <span>Choose Image/GIF</span>
+      </button>
+    </>
   )
 }
 
