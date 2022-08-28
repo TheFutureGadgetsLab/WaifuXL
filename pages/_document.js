401158b54f779caf0396d58a1b370eb41f0abd88
Haydn Jones <haydnjonest@gmail.com>
2022-08-28 11:13:47 -0600

    Testing OpenGraph cards

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,14 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/images/icon.webp"></link>
           <meta name="theme-color" content="#fff" />
+          <link rel="icon" type="image/png" href="/images/icon.webp" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Title" />
+          <meta property="og:description" content="Description" />
+          <meta property="og:site_name" content="Site Name" />
+          <meta property="og:url" content="https://waifuxl.com/" />
+          <meta property="og:image" content="https://haydn.fgl.dev/posts/the-launch-of-waifuxl/images/WaifuXL.webp" />
         </Head>
         <body>
           <Main />
