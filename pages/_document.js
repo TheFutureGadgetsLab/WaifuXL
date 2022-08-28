e946748b5d7c0eac4bc38491a920a68a16f7b555
Haydn Jones <haydnjonest@gmail.com>
2022-08-28 11:29:24 -0600

    Update OG

--- pages/_document.js
+++ pages/_document.js
@@ -11,11 +11,13 @@ class MyDocument extends Document {
           <link rel="icon" type="image/png" href="/images/icon.webp" />
           <link rel="manifest" href="/manifest.json" />
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="Title" />
-          <meta property="og:description" content="Description" />
-          <meta property="og:site_name" content="Site Name" />
+          <meta property="og:title" content="SOTA Anime Image Super Resolution" />
+          <meta property="og:description" content="High quality image upsampling for anime-style art using state-of-the-art neural networks, directly in your browser." />
+          <meta property="og:site_name" content="WaifuXL" />
           <meta property="og:url" content="https://waifuxl.com/" />
           <meta property="og:image" content="https://haydn.fgl.dev/posts/the-launch-of-waifuxl/images/WaifuXL.webp" />
+          <meta property="og:image:width" content="2347" />
+          <meta property="og:image:height" content="1277" />
         </Head>
         <body>
           <Main />
