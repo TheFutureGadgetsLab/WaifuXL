c0e1fc986831523f4c1c95f0b79f9e36af3ecfd1
Haydn Jones <haydnjonest@gmail.com>
2022-11-06 09:44:05 -0700

    Break out styling in ImageDisplayComponent

--- components/ImageDisplayComponent.jsx
+++ components/ImageDisplayComponent.jsx
@@ -6,58 +6,27 @@ const ImageDisplay = () => {
   const mobile = useAppStateStore((state) => state.mobile)
   const outputURI = useImageStore((state) => state.outputURI)
   const inputURI = useImageStore((state) => state.inputURI)
+
+  const styling = {
+    maxHeight: mobile ? '100vw' : '',
+    maxWidth: mobile ? '100vw' : '',
+    minHeight: mobile ? '100vw' : '',
+    minWidth: mobile ? '100vw' : '',
+    width: mobile ? 'auto' : 'auto',
+    height: mobile ? 'auto' : '70vh',
+    objectFit: mobile ? 'cover' : '',
+  }
   return (
     <div
       className={`items-center flex justify-center drop-shadow-md overflow-hidden ml-5 mr-5 ${!mobile ? 'w-full' : ''}`}
     >
       {outputURI == null ? (
-        <Image
-          src={inputURI}
-          width="1"
-          height="1"
-          style={{
-            maxHeight: mobile ? '100vw' : '',
-            maxWidth: mobile ? '100vw' : '',
-            minHeight: mobile ? '100vw' : '',
-            minWidth: mobile ? '100vw' : '',
-            width: mobile ? 'auto' : 'auto',
-            height: mobile ? 'auto' : '70vh',
-            objectFit: mobile ? 'cover' : '',
-          }}
-        />
+        <Image src={inputURI} width="1" height="1" style={styling} />
       ) : (
         <ReactCompareSlider
           position={50}
-          itemOne={
-            <ReactCompareSliderImage
-              src={inputURI}
-              style={{
-                maxHeight: mobile ? '100vw' : '',
-                maxWidth: mobile ? '100vw' : '',
-                minHeight: mobile ? '100vw' : '',
-                minWidth: mobile ? '100vw' : '',
-                width: mobile ? 'auto' : 'auto',
-                height: mobile ? 'auto' : '70vh',
-                objectFit: mobile ? 'cover' : '',
-              }}
-              alt="Image one"
-            />
-          }
-          itemTwo={
-            <ReactCompareSliderImage
-              src={outputURI}
-              style={{
-                maxHeight: mobile ? '100vw' : '',
-                maxWidth: mobile ? '100vw' : '',
-                minHeight: mobile ? '100vw' : '',
-                minWidth: mobile ? '100vw' : '',
-                width: mobile ? 'auto' : 'auto',
-                height: mobile ? 'auto' : '70vh',
-                objectFit: mobile ? 'cover' : '',
-              }}
-              alt="Image two"
-            />
-          }
+          itemOne={<ReactCompareSliderImage src={inputURI} style={styling} alt="Before image" />}
+          itemTwo={<ReactCompareSliderImage src={outputURI} style={styling} alt="After image" />}
         />
       )}
     </div>
