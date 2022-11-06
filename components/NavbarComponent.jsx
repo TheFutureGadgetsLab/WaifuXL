f50c73d3a0aabe92846c7a3c9297e36626317c9f
Haydn Jones <haydnjonest@gmail.com>
2022-11-06 10:18:12 -0700

    Using link from NextJS for navigation

--- components/NavbarComponent.jsx
+++ components/NavbarComponent.jsx
@@ -1,4 +1,4 @@
-// USE NEXTJS LINKS FOR NAVIGATION
+import Link from 'next/link'
 
 const NavbarComponent = ({ currentPage }) => {
   return (
@@ -8,31 +8,31 @@ const NavbarComponent = ({ currentPage }) => {
           <div className="container mx-auto py-4 flex justify-between items-center">
             <div className="flex gap-4">
               <div className="grow flex items-center">
-                <a
+                <Link
                   href="/about"
                   className={`text-2xl font-semibold text-black cursor-pointer ${
                     currentPage === 'about' ? 'underline' : ''
                   }`}
                 >
                   About
-                </a>
+                </Link>
               </div>
-              <div className="grow text-3xl font-bold cursor-pointer text-pink">
-                <a className={`${currentPage === 'index' ? 'underline' : ''}`} href="/">
-                  WaifuXL
-                </a>
-              </div>
-
-              <div className="grow flex items-center">
-                <a
-                  href="/donate"
-                  className={`text-2xl font-semibold text-black cursor-pointer ${
-                    currentPage === 'donate' ? 'underline' : ''
-                  }`}
-                >
-                  Donate
-                </a>
-              </div>
+              <Link
+                href="/"
+                className={`grow text-3xl font-bold cursor-pointer text-pink ${
+                  currentPage === 'index' ? 'underline' : ''
+                }`}
+              >
+                WaifuXL
+              </Link>
+              <Link
+                href="/donate"
+                className={`text-2xl font-semibold text-black cursor-pointer ${
+                  currentPage === 'about' ? 'underline' : ''
+                }`}
+              >
+                Donate
+              </Link>
             </div>
           </div>
         </nav>
