38338d80eb13654a6b33b13b3a0550d142d7e594
Haydn Jones <supa@pop-os.localdomain>
2022-11-29 18:05:51 -0600

    Delete unused clipboard function

--- services/imageUtilities.js
+++ services/imageUtilities.js
@@ -45,33 +45,6 @@ function downloadImage(fileName, extension, outputURI) {
 }
 
 /**
- * Copies a Data URI to the clipboard. Note that since Chrome only implementes PNGs in the Clipboard API
- * we need to convert all input images to PNGs before being copied (realistically only an edge case when
- * using the landing page image since it's stored as a webp)
- *
- * @param {String} outputURI
- */
-function copyImageToClipboard(outputURI) {
-  const img = new Image()
-  img.src = outputURI
-  img.crossOrigin = 'Anonymous'
-  img.onload = function () {
-    //Convert image to PNG (needs to be done if the input image is anything other than PNG)
-    const canvas = document.createElement('canvas')
-    canvas.width = img.width
-    canvas.height = img.height
-    const context = canvas.getContext('2d')
-    context.drawImage(img, 0, 0)
-    canvas.toBlob(function (blob) {
-      var item = {
-        'image/png': blob,
-      }
-      navigator.clipboard.write([new ClipboardItem(item)])
-    }, 'image/png')
-  }
-}
-
-/**
  * Get data URI from image, passing it to a callback
  *
  * @param {File} fileObj File object from file upload or paste event
@@ -145,4 +118,4 @@ async function uploadToImgur(dataURI) {
     })
 }
 
-export { getDataURIFromInput, downloadImage, setDataURIFromFile, copyImageToClipboard, uploadToImgur }
+export { getDataURIFromInput, downloadImage, setDataURIFromFile, uploadToImgur }
