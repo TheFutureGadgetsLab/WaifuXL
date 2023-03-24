08944233f3288523ac5bdc87c1635260e0183af3
Haydn Jones <haydnjonest@gmail.com>
2023-03-23 19:45:19 -0600

    waifu2x dunk

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,7 @@ class MyDocument extends Document {
           <meta property="og:title" content="WaifuXL | SOTA Anime Image Super Resolution" />
           <meta
             property="og:description"
-            content="High quality image upsampling for anime-style art using state-of-the-art neural networks, directly in your browser."
+            content="High quality image upsampling for anime-style art using state-of-the-art neural networks, directly in your browser. Better than waifu2x."
           />
           <meta property="og:site_name" content="WaifuXL" />
           <meta property="og:url" content="https://waifuxl.com/" />
