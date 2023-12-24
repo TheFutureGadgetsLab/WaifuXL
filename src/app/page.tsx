7c5d68479b623b7d27e6a30c39fd121bc2584fed
Haydn Jones <haydnjonest@gmail.com>
2023-12-24 15:28:51 -0500

    Kofi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
 
 export default function HomePage() {
   return (
@@ -11,12 +10,22 @@ export default function HomePage() {
           flexDirection: 'column',
           justifyContent: 'center',
           alignItems: 'center',
+          ml: '300px',
         }}
       >
-        <Typography variant="h1" gutterBottom>
-          Home Page
-        </Typography>
+        <iframe
+          id="kofiframe"
+          src="https://ko-fi.com/thefuturegadgetslab/?hidefeed=true&widget=true&embed=true&preview=true"
+          style={{
+            border: 'none',
+            padding: '0px',
+            overflow: 'hidden',
+            background: 'rgba(255, 255, 255, 0.0)',
+          }}
+          height="712"
+          title="thefuturegadgetslab"
+        ></iframe>
       </Box>
     </Container>
-  );
+  )
 }
