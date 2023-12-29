f7c4ffc1684117f9ef61debefee77d865165a731
Haydn Jones <haydnjonest@gmail.com>
2023-12-28 23:37:05 -0700

    Formatting

--- src/components/ThemeRegistry/EmotionCache.tsx
+++ src/components/ThemeRegistry/EmotionCache.tsx
@@ -1,9 +1,12 @@
 'use client'
+
 import * as React from 'react'
+
+import type { EmotionCache, Options as OptionsOfCreateCache } from '@emotion/cache'
+
+import { CacheProvider as DefaultCacheProvider } from '@emotion/react'
 import createCache from '@emotion/cache'
 import { useServerInsertedHTML } from 'next/navigation'
-import { CacheProvider as DefaultCacheProvider } from '@emotion/react'
-import type { EmotionCache, Options as OptionsOfCreateCache } from '@emotion/cache'
 
 export type NextAppDirEmotionCacheProviderProps = {
   /** This is the options passed to createCache() from 'import createCache from "@emotion/cache"' */
--- src/components/ThemeRegistry/ThemeRegistry.tsx
+++ src/components/ThemeRegistry/ThemeRegistry.tsx
@@ -1,8 +1,10 @@
 'use client'
+
 import * as React from 'react'
-import { ThemeProvider } from '@mui/material/styles'
+
 import CssBaseline from '@mui/material/CssBaseline'
 import NextAppDirEmotionCacheProvider from './EmotionCache'
+import { ThemeProvider } from '@mui/material/styles'
 import theme from './theme'
 
 export default function ThemeRegistry({ children }: { children: React.ReactNode }) {
