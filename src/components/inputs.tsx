6b58a503a11a70cee4cef37e0e52de38c52ff299
Alec Benson <alecbenson21@gmail.com>
2023-12-29 16:43:51 -0700

    Moved buttons to a new component so they can be resued

--- src/components/inputs.tsx
+++ src/components/inputs.tsx
@@ -1,55 +1,173 @@
-import { FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, SvgIconTypeMap } from '@mui/material'
-import { MouseEventHandler, ReactNode } from 'react'
-
+import { Button, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from '@mui/material'
+import { downloadImage } from '@/services/imageUtilities'
+import { CloudDownload, CloudUpload, CopyAll, RunCircle } from '@mui/icons-material'
+import { useAppStateStore, useImageStore } from '@/services/useState'
+import { initializeONNX, upScaleFromURI } from '@/services/inference/utils'
 import ButtonComponent from './button'
-import { OverridableComponent } from '@mui/material/OverridableComponent'
+import { ReactNode } from 'react'
+import { text } from 'stream/consumers'
 
-interface inputSpecType {
-  key: string
-  text: string
-  func: MouseEventHandler<HTMLButtonElement> | ((event: SelectChangeEvent<any>, child: ReactNode) => void) | undefined
-  icon: OverridableComponent<SvgIconTypeMap<{}, 'svg'>>
-  display: boolean
-  disabled: boolean
-  type: string
-  current?: any
+const ModalUpload = () => {
+  const { setOutputURI, setUpscaleFactor, setTags, inputURI, extension, upscaleFactor, outputURI, fileName, hasntRun } =
+    useImageStore()
+  const { setDownloadReady, setRunning, setErrorMessage, running, setInputModalOpen } = useAppStateStore()
+  return (
+    <ButtonComponent
+      item_key="ModalUpload"
+      text="Choose Image / GIF"
+      func={() => {
+        setInputModalOpen(true)
+      }}
+      Icon={CloudUpload}
+      disabled={running}
+    />
+  )
+}
+const DownloadImage = () => {
+  const { extension, outputURI, fileName, hasntRun } = useImageStore()
+
+  if (outputURI != null) {
+    return (
+      <ButtonComponent
+        item_key={'DownloadImage'}
+        text={'Download'}
+        func={() => {
+          if (outputURI != null) {
+            downloadImage(fileName, extension, outputURI)
+          }
+        }}
+        Icon={CloudDownload}
+        disabled={hasntRun}
+      />
+    )
+  } else {
+    return <></>
+  }
+}
+const CopyImage = () => {
+  const { extension, outputURI, hasntRun } = useImageStore()
+  if (outputURI != null) {
+    return (
+      <ButtonComponent
+        item_key={'Copy'}
+        text={'Copy to Clipboard'}
+        func={() => {
+          if (outputURI != null) {
+            copyImg(outputURI)
+          }
+        }}
+        Icon={CopyAll}
+        disabled={hasntRun || extension == 'gif'}
+      />
+    )
+  } else {
+    return <></>
+  }
+}
+const RunModel = () => {
+  const { setOutputURI, setUpscaleFactor, setTags, inputURI, extension, upscaleFactor, outputURI, fileName, hasntRun } =
+    useImageStore()
+  const { setDownloadReady, setRunning, setErrorMessage, running, setInputModalOpen } = useAppStateStore()
+  if (outputURI == null) {
+    return (
+      <ButtonComponent
+        item_key="RunComponent"
+        text="Run"
+        func={() => {
+          pipeline(
+            setOutputURI,
+            setUpscaleFactor,
+            setTags,
+            inputURI,
+            extension,
+            upscaleFactor,
+            setDownloadReady,
+            setRunning,
+            setErrorMessage,
+          )
+        }}
+        Icon={RunCircle}
+        disabled={running}
+      />
+    )
+  } else {
+    return <></>
+  }
+}
+const UpscaleFactor = () => {
+  const { setOutputURI, setUpscaleFactor, setTags, inputURI, extension, upscaleFactor, outputURI, fileName, hasntRun } =
+    useImageStore()
+  const { setDownloadReady, setRunning, setErrorMessage, running, setInputModalOpen } = useAppStateStore()
+  if (outputURI == null) {
+    return (
+      <FormControl key={'UpscaleFactor'} fullWidth>
+        <InputLabel>Factor</InputLabel>
+        <Select
+          disabled={running}
+          value={upscaleFactor.toString()}
+          color="primary"
+          label={'Upscale Factor'}
+          onChange={(e: SelectChangeEvent) => {
+            console.log(e.target.value)
+            console.log(upscaleFactor)
+            setUpscaleFactor(parseInt(e.target.value))
+          }}
+        >
+          <MenuItem value={1}>2</MenuItem>
+          <MenuItem value={2}>4</MenuItem>
+          <MenuItem value={4}>8</MenuItem>
+        </Select>
+      </FormControl>
+    )
+  } else {
+    return <></>
+  }
 }
 
-interface inputsType {
-  buttonSpecs: inputSpecType[]
+async function copyImg(src: string) {
+  const img = await fetch(src)
+  const imgBlob = await img.blob()
+  try {
+    navigator.clipboard.write([
+      new ClipboardItem({
+        'image/png': imgBlob, // change image type accordingly
+      }),
+    ])
+  } catch (error) {
+    console.error(error)
+  }
 }
-export default function InputsComponent({ buttonSpecs }: inputsType) {
-  const toReturn = buttonSpecs
-    .filter((info: inputSpecType) => info.display)
-    .map(({ key, text, func, icon: Icon, display, disabled, type, current }) => {
-      if (type == 'button') {
-        return (
-          <ButtonComponent
-            key={(Math.random() + 1).toString(36).substring(7)}
-            item_key={key}
-            text={text}
-            func={func as MouseEventHandler<HTMLButtonElement>}
-            Icon={Icon}
-            disabled={disabled}
-          />
-        )
-      } else if (type == 'selection') {
-        return (
-          <FormControl key={key} fullWidth>
-            <InputLabel>Factor</InputLabel>
-            <Select
-              value={current}
-              color="primary"
-              label={text}
-              onChange={func as (event: SelectChangeEvent<any>, child: ReactNode) => void}
-            >
-              <MenuItem value={1}>2</MenuItem>
-              <MenuItem value={2}>4</MenuItem>
-              <MenuItem value={4}>8</MenuItem>
-            </Select>
-          </FormControl>
-        )
+
+async function pipeline(
+  setOutputURI: (uri: string) => void,
+  setUpscaleFactor: (factor: number) => void,
+  setTags: (tags: any) => void,
+  inputURI: string,
+  extension: string,
+  upscaleFactor: number,
+  setDownloadReady: (ready: boolean) => void,
+  setRunning: (running: boolean) => void,
+  setErrorMessage: (message: string) => void,
+) {
+  try {
+    await initializeONNX()
+    setRunning(true)
+
+    try {
+      const result = await upScaleFromURI(extension, setTags, inputURI, upscaleFactor)
+      if (result != null) {
+        setOutputURI(result)
       }
-    })
-  return <>{toReturn}</>
+    } catch (error) {
+      setErrorMessage('Failed to upscale image.')
+    } finally {
+      setDownloadReady(true)
+      setRunning(false)
+      setUpscaleFactor(1)
+    }
+  } catch (error) {
+    setErrorMessage('Could not load model.')
+  }
 }
+
+export { ModalUpload, DownloadImage, CopyImage, RunModel, UpscaleFactor }
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,111 +1,14 @@
 'use client'
 
-import { CloudDownload, CloudUpload, CopyAll, RunCircle } from '@mui/icons-material'
-import { initializeONNX, upScaleFromURI } from '@/services/inference/utils'
-import { useAppStateStore, useImageStore } from '../services/useState'
-
-import ButtonsComponent from './inputs'
+import { useImageStore } from '../services/useState'
+import { ModalUpload, DownloadImage, CopyImage, RunModel, UpscaleFactor } from '@/components/inputs'
 import Drawer from '@mui/material/Drawer'
-import { SelectChangeEvent } from '@mui/material'
 import { TagDisplayComponent } from '@/components/tagDisplay'
-import { downloadImage } from '@/services/imageUtilities'
 
 const DRAWER_WIDTH = 300
 
 export default function SideBarComponent() {
-  const {
-    setOutputURI,
-    setUpscaleFactor,
-    setTags,
-    inputURI,
-    extension,
-    upscaleFactor,
-    outputURI,
-    fileName,
-    hasntRun,
-    tags,
-  } = useImageStore()
-  const { setDownloadReady, setRunning, setErrorMessage, running, setInputModalOpen } = useAppStateStore()
-
-  const sidebarContent = [
-    {
-      key: 'ModalUpload',
-      text: 'Choose Image / GIF',
-      func: () => {
-        setInputModalOpen(true)
-      },
-      icon: CloudUpload,
-      display: true,
-      disabled: running,
-      type: 'button',
-      current: undefined,
-    },
-    {
-      key: 'DownloadImage',
-      text: 'Download',
-      func: () => {
-        if (outputURI != null) {
-          downloadImage(fileName, extension, outputURI)
-        }
-      },
-      icon: CloudDownload,
-      display: outputURI != null,
-      disabled: hasntRun,
-      type: 'button',
-      current: undefined,
-    },
-    {
-      key: 'Copy',
-      text: 'Copy to Clipboard',
-      func: () => {
-        if (outputURI != null) {
-          copyImg(outputURI)
-        }
-      },
-      icon: CopyAll,
-      display: outputURI != null,
-      disabled: hasntRun || extension == 'gif',
-      type: 'button',
-      current: undefined,
-    },
-    {
-      key: 'RunComponent',
-      text: 'Run',
-      func: () => {
-        pipeline(
-          setOutputURI,
-          setUpscaleFactor,
-          setTags,
-          inputURI,
-          extension,
-          upscaleFactor,
-          setDownloadReady,
-          setRunning,
-          setErrorMessage,
-        )
-      },
-      icon: RunCircle,
-      display: outputURI == null,
-      disabled: running,
-      type: 'button',
-      current: undefined,
-    },
-    {
-      key: 'UpscaleFactor',
-      text: 'Upscale Factor',
-      func: (e: SelectChangeEvent) => {
-        console.log(e.target.value)
-        console.log(upscaleFactor)
-        setUpscaleFactor(parseInt(e.target.value))
-      },
-      icon: CopyAll,
-      display: outputURI == null,
-      disabled: running,
-      type: 'selection',
-      current: upscaleFactor,
-    },
-  ]
-
+  const { tags } = useImageStore()
   return (
     <Drawer
       sx={{
@@ -120,60 +23,19 @@ export default function SideBarComponent() {
           padding: 2,
           bgcolor: 'primary',
         },
+        display: { xs: 'none', sm: 'block' },
       }}
       open={true}
       variant="persistent"
     >
-      <ButtonsComponent buttonSpecs={sidebarContent} />
+      <ModalUpload />
+      <DownloadImage />
+      <CopyImage />
+      <RunModel />
+      <UpscaleFactor />
       <TagDisplayComponent title={'Top Chars'} tags={tags.topChars} />
       <TagDisplayComponent title={'Top Desc'} tags={tags.topDesc} />
       <TagDisplayComponent title={'Explicitness'} tags={tags.rating} />
     </Drawer>
   )
-}
-
-async function copyImg(src: string) {
-  const img = await fetch(src)
-  const imgBlob = await img.blob()
-  try {
-    navigator.clipboard.write([
-      new ClipboardItem({
-        'image/png': imgBlob, // change image type accordingly
-      }),
-    ])
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-async function pipeline(
-  setOutputURI: (uri: string) => void,
-  setUpscaleFactor: (factor: number) => void,
-  setTags: (tags: any) => void,
-  inputURI: string,
-  extension: string,
-  upscaleFactor: number,
-  setDownloadReady: (ready: boolean) => void,
-  setRunning: (running: boolean) => void,
-  setErrorMessage: (message: string) => void,
-) {
-  try {
-    await initializeONNX()
-    setRunning(true)
-
-    try {
-      const result = await upScaleFromURI(extension, setTags, inputURI, upscaleFactor)
-      if (result != null) {
-        setOutputURI(result)
-      }
-    } catch (error) {
-      setErrorMessage('Failed to upscale image.')
-    } finally {
-      setDownloadReady(true)
-      setRunning(false)
-      setUpscaleFactor(1)
-    }
-  } catch (error) {
-    setErrorMessage('Could not load model.')
-  }
 }
