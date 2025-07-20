2691818867656aec1f873ba0d39d461de7358a3e
Haydn Jones <haydnjonest@gmail.com>
2025-07-20 15:57:30 -0400

    Update nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   productionBrowserSourceMaps: true,
   images: { unoptimized: true },
   output: process.env.NODE_ENV === 'development' ? 'standalone' : 'export',
