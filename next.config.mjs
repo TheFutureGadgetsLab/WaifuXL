35080f461dc40ddf11556d932a38f625d70b8c31
Haydn Jones <haydnjonest@gmail.com>
2026-01-05 15:26:21 -0500

    Try fix kofi issue

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,7 @@ const nextConfig = {
         ],
       },
       {
-        source: '/donate',
+        source: '/donate/:path*',
         headers: [
           { key: 'Cross-Origin-Embedder-Policy', value: 'unsafe-none' },
           { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
